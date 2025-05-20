3cf64808b3fcd7a20517919443e30e9e06c464d7
juansesu3 <juanseu3@hotmail.com>
2025-05-20 10:37:45 +0200

    optimizing seo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import QuoteCard from './Components/QuoteCard'
-
+import { Helmet } from 'react-helmet-async';
 function App() {
-
 
   return (
     <>
+     <Helmet>
+
+     <title>Inspirational Quotes</title>
+     <meta name="description" content="Discover motivational quotes to brighten your day with PandorAI." />
+  <meta name="keywords" content="quotes, inspiration, motivation" />
+  <meta property="og:title" content="Inspirational Quotes" />
+  <meta property="og:description" content="Find motivational quotes and optimize your day." />
+  <meta property="og:type" content="website" />
+      </Helmet>
    <QuoteCard/>
     </>
   )
